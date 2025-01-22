5e802e22f2237a5057b02dc5f0bd9c07a20217c4
jyam12 <138462074+jyam12@users.noreply.github.com>
2025-01-22 08:59:26 +0800

    Add SVR (may not be use)

---
+++ ML/svr.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.svm import SVR
+import joblib
+
+class svr:
+    # Parse and preprocess relevant columns for all levels
+    def __init__(self,X_train,y_train,X_eval):
+
+        self.X_train = X_train.copy(deep=True)
+        self.y_train = y_train.copy(deep=True)
+        self.X_eval = X_eval.copy(deep=True)
+
+    # Train Linear Regression model
+    def train(self):
+        svr_model = SVR(kernel='rbf', )
+        svr_model.fit(self.X_train, self.y_train)
+        # Save the models
+        joblib.dump(svr_model, "ML/svr_model.pkl") 
+
+    def predict(self):
+        svr_model = joblib.load("ML/svr_model.pkl")
+        return svr_model.predict(self.X_eval)
